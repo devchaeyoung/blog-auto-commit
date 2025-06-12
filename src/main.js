bd2d41fbc69e6c5d6236740b62aa48f4893fbcdd
devchaeyoung <chaemaa00@gmail.com>
2025-06-12 17:02:51 +0900

    feat: 소개 페이지 추가

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const root = document.getElementById('root');
+
+const title = document.createElement('h1');
+title.textContent = 'Blog Auto Commit';
+
+root.appendChild(title);
+
+const description = document.createElement('p');
+description.textContent = 'This is a blog auto commit repository';
+
+root.appendChild(description);
